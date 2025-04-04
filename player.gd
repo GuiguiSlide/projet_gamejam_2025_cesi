d0650a185b1ec135a6d88d8b384f41b92de54492
Guillaume Beaufort <guillaume.beaufort@viacesi.fr>
2025-04-04 22:52:53 +0200

    day1 end

--- player.gd
+++ player.gd
@@ -1,44 +1,44 @@
 extends CharacterBody3D
 
-
-const SPEED = 10
+const SPEED = 10.0
 const JUMP_VELOCITY = 4.5
-var angspeed=10
-var mouse_sens := 0.25
+const MOUSE_SENS = 0.25
 
+# Node References
+@onready var cam: Node3D = $arm
+@onready var gun_anim: AnimationPlayer = $arm/pistol/AnimationPlayer
 
-@onready var cross: MeshInstance3D= %arm/MeshInstance3D
-@onready var cam: Node3D= %arm
-@onready var gun_anim =$arm/pistol/AnimationPlayer
+# Gravity as a scalar (float)
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")  # Directly get gravity value
 
-var _camera_input_direction := Vector2.ZERO
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
-func _input(event: InputEvent) -> void:
+func _input(event: InputEvent):
 	if event.is_action_pressed("leftclick"):
 		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-	if event.is_action_pressed("ui_cancel"):
+	if event.is_action_pressed("ui_accept"):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-		
-func _unhandled_input(event: InputEvent) -> void:
-	var is_camera_motion :=(
-		event is InputEventMouseMotion and
-		Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED
-	)
-	if is_camera_motion:
-		_camera_input_direction = event.screen_relative * mouse_sens
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
 
-	# Handle jump.
+func _unhandled_input(event: InputEvent):
+	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		cam.rotation.x -= deg_to_rad(event.relative.y * MOUSE_SENS)
+		cam.rotation.x = clamp(cam.rotation.x, deg_to_rad(-90), deg_to_rad(90))
+		rotate_y(deg_to_rad(-event.relative.x * MOUSE_SENS))
+
+func _physics_process(delta):
+	# Apply gravity (as a float to velocity.y)
+	if not is_on_floor():
+		velocity.y -= gravity * delta  # Now uses scalar gravity
+
+	# Jumping
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (cam.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	# Movement
+	var input_dir = Input.get_vector("left", "right", "foward", "backward")
+	var direction = (cam.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
@@ -47,12 +47,7 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
-	
-	cam.rotation.x -= _camera_input_direction.y*delta
-	cam.rotation.x= clamp(cam.rotation.x, -PI / 3, PI/2)
-	cam.rotation.y -= _camera_input_direction.x*delta
 
-	_camera_input_direction = Vector2.ZERO
-	
-	if Input.is_action_just_pressed('leftclick'):
+	# Shooting
+	if Input.is_action_just_pressed("leftclick") and gun_anim:
 		gun_anim.play("shoot")
